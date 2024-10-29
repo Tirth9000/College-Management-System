6bda19c41d0e57b7451c78f3c834ebcb70a67534
Tirth_ <128807701+Tirth9000@users.noreply.github.com>
2024-10-29 16:39:00 +0530

    Add files via upload

---
+++ StudentManagerApp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
+import os
+
+# Create your models here.
+
+def delete(self, *args, **kwargs):
+    if self.photo:
+        if os.path.isfile(self.photo.path):
+            os.remove(self.photo.path)
+    super().delete(*args, **kwargs)
---
+++ StudentManagerProject/urls.py
@@ -0,0 +1,45 @@
+"""
+URL configuration for StudentManagerProject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from auth_app.views import *
+from StudentManagerApp.views import *
+from StudentManagerProject import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', LandingPageWithRegistration, name='landing_register'),
+    path('login-page/', UserLogin, name='login_page'),
+    path('student-home/<str:id>/', StudentHome, name='student_home_page'),
+    path('student-home/<str:id>/home/', StudentHome, name='student_home_page'),
+    path('student-home/<str:id>/about/', StudentAbout, name='student_about_page'),
+    path('student-home/<str:id>/contact/', StudentContact, name='student_contact_page'),
+    path('admin-home/<str:id>/', AdminHome, name='admin_home'),
+    path('admin-home/<str:id>/home/', AdminHome, name='admin_home_page'),
+    path('admin-home/<str:id>/about/', AdminAbout, name='admin_about_page'),
+    path('admin-home/<str:id>/contact/', AdminContact, name='admin_contact_page'),
+    
+    path('admin-home/<str:id>/all-students/', AllStudents, name='all_students'),
+    path('admin-home/<str:id>/all-students/update-student/<str:std_id>/', UpdateStudent, name='update_student'),
+    path('admin-home/<str:id>/all-students/delete-student/<str:std_id>/', DeleteStudent, name='delete_student'),
+
+    path('logout/', Logout, name='logout'),
+    
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
