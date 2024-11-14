cd91a8c0af40412a562c1643bb379f0ee48509b4
Tirth_ <128807701+Tirth9000@users.noreply.github.com>
2024-11-15 01:50:08 +0530

    Delete StudentManagerProject directory

--- StudentManagerProject/__init__.py
+++
@@ -1,8 +0,0 @@
-# myproject/__init__.py
-from __future__ import absolute_import, unicode_literals
-
-# This will make sure the app is always imported when Django starts
-from .celery import app 
-
-__all__ = ["app"]
-
